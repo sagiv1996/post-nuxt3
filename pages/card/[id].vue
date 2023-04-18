cc333076e84f845250b2b75462b03282d18c2203
sagiv bonali <sagivb13@gmail.com>
2023-04-18 18:38:51 +0300

    Add a custom page for post

---
+++ pages/card/[id].vue
@@ -0,0 +1,12 @@
+<template>
+    <template v-if="pending"> Loading...</template>
+    <template v-else-if="error">Error</template>
+    <post-card :action="{ label: 'back', path: '/' }" v-else-if="posts" :post="posts[0]" />
+</template>
+<script setup lang="ts">
+const route = useRoute()
+const { data: posts, pending, error } = await useFetch<Post[]>('https://jsonplaceholder.typicode.com/posts', {
+    query: { id: route.params.id },
+
+})
+</script>
