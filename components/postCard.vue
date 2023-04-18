d5853215cc4a472d7fbfa44825496edd788bb50e
sagiv bonali <sagivb13@gmail.com>
2023-04-18 17:43:03 +0300

    Create a basic component to shot a post

---
+++ components/postCard.vue
@@ -0,0 +1,17 @@
+<template>
+    <v-card>
+        <v-card-title> {{ post.title }}</v-card-title>
+
+        <v-card-text>
+            {{ post.body }}
+        </v-card-text>
+    </v-card>
+</template>
+<script setup lang="ts">
+
+interface Props {
+    post: Post
+
+}
+const { post } = defineProps<Props>()
+</script>
