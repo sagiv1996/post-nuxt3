6662a62171d87f3e0aa0b42b78dcab13eedc549f
sagiv bonali <sagivb13@gmail.com>
2023-04-18 16:21:06 +0300

    Initial nuxt3 project

---
+++ components/seyHello.vue
@@ -0,0 +1,3 @@
+<template>
+    <div>Hello for all pepole!</div>
+</template>
---
+++ pages/index.vue
@@ -0,0 +1,6 @@
+<template>
+    <v-card>
+        <v-card-title>Hello </v-card-title>
+        <sey-hello />
+    </v-card>
+</template>
