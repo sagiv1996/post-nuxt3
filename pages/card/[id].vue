084287a7ab25212971a673675a564135caf3df9c
sagiv bonali <sagivb13@gmail.com>
2023-04-18 20:17:25 +0300

    Get only the post from posts

--- pages/card/[id].vue
+++ pages/card/[id].vue
@@ -2,13 +2,17 @@
     <template v-if="pending"> Loading...</template>
     <template v-else-if="error">Error</template>
     <post-card
-        :actions="[{ label: 'back', path: '/' }, { label: 'More Posts by this user', path: `/card/user-id-${posts[0].userId}` }]"
-        v-else-if="posts" :post="posts[0]" />
+        :actions="[{ label: 'back', path: '/' }, { label: 'More Posts by this user', path: `/card/user-id-${post.userId}` }]"
+        v-else-if="post" :post="post" />
 </template>
+
 <script setup lang="ts">
 const route = useRoute()
-const { data: posts, pending, error } = await useFetch<Post[]>('https://jsonplaceholder.typicode.com/posts', {
-    query: { id: route.params.id },
+const { data: posts, pending, error } = await useAsyncData<Post[]>(() => $fetch('https://jsonplaceholder.typicode.com/posts', {
+    query: { id: route.params.id }
+}));
 
-})
+const post = computed<Post | undefined>(() => posts?.value?.[0]);
+
+
 </script>
