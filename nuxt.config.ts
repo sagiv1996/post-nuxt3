0acaabd09d3ccc0517901ba37989571985b85fc4
sagiv bonali <sagivb13@gmail.com>
2023-04-19 09:10:14 +0300

    Add the apiBase for config file

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,3 +1,8 @@
 export default defineNuxtConfig({
+  runtimeConfig: {
+    public: {
+      apiBase: "https://jsonplaceholder.typicode.com",
+    },
+  },
   modules: ["@invictus.codes/nuxt-vuetify"],
 });
